23befdd1e93b65f0d7a0f75e5b57b6537f616f67
AJaeck <ajaeck@gmail.com>
2023-10-28 16:19:01 +0200

    PD App frontend

---
+++ src/NewTest.js
@@ -0,0 +1,14 @@
+// src/NewTest.js
+import React from 'react';
+import TappingTest from './components/TappingTest';
+
+function NewTest() {
+    return (
+       <div className="App">
+           <h1>Bradykinesia Tapping Test</h1>
+           <TappingTest />
+       </div>
+    );
+}
+
+export default NewTest;
