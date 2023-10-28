2e158f2bfa8c9a8a8411a42c6dc91042872f29c2
AJaeck <ajaeck@gmail.com>
2023-10-28 16:22:44 +0200

    Revert "PD App frontend"
    This reverts commit 23befdd1e93b65f0d7a0f75e5b57b6537f616f67.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import CreateNewUser from './CreateNewUser';
-import UserProfile from './UserProfile';
-import LoadUserProfile from './LoadUserProfile';
-import NewTest from './NewTest';
-import Results from "./Results";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/create-user" element={<CreateNewUser />} />
-                <Route path="/load-user" element={<LoadUserProfile />} />
-                <Route path="/user-profile/:userId" element={<UserProfile />} />
-                <Route path="/new-test" element={<NewTest />} />
-                <Route path="/results" element={<Results />} />
-                {/* ... other routes */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
-
